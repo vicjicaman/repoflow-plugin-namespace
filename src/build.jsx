ab24d22c739cbe323d4a8b98fe2f85a86e6d5c18
Victor Jimenez <vic.jicama@gmail.com>
2019-09-30 17:24:25 +0200

    Separare and encapsulate Kube entities from the plugins

--- src/build.jsx
+++ src/build.jsx
@@ -3,35 +3,11 @@ import fs from "fs-extra";
 import path from "path";
 import YAML from "yamljs";
 import { exec, spawn, wait } from "@nebulario/core-process";
-import {
-  Operation,
-  IO,
-  Watcher,
-  Performer
-} from "@nebulario/core-plugin-request";
+import { Operation, IO, Watcher } from "@nebulario/core-plugin-request";
 import * as Config from "@nebulario/core-config";
 import * as Cluster from "@nebulario/core-cluster";
 import * as JsonUtils from "@nebulario/core-json";
-import { isFile, getFiles } from "./utils";
-
-const configFolder = (base, src, dest, values, cxt) => {
-  const srcBase = path.join(src, base);
-  const destBase = path.join(dest, base);
-  const list = getFiles(srcBase, "yaml");
-
-  for (const vPath of list) {
-    const file = path.basename(vPath);
-    IO.sendEvent(
-      "info",
-      {
-        data: "Configure " + base + " - " + file
-      },
-      cxt
-    );
-
-    Cluster.Config.configure(file, srcBase, destBase, values);
-  }
-};
+import * as Performer from "@nebulario/core-performer";
 
 export const clear = async (params, cxt) => {
   const {
@@ -66,21 +42,13 @@ export const init = async (params, cxt) => {
   } = params;
 
   if (type === "instanced") {
-    Performer.link(performer, performers, {
-      onLinked: depPerformer => {
-        if (depPerformer.module.type === "config") {
-          IO.sendEvent(
-            "info",
-            {
-              data: depPerformer.performerid + " config linked!"
-            },
-            cxt
-          );
+    Performer.linked(performer, performers)
+      .filter(({ module: { type } }) => type === "config")
+      .forEach(({ performerid }) => {
+        IO.print("info", performerid + " config linked!", cxt);
+        Config.link(folder, performerid);
+      });
 
-          Config.link(folder, depPerformer.performerid);
-        }
-      }
-    });
     await Config.init(folder);
   }
 };
@@ -101,45 +69,22 @@ export const start = (params, cxt) => {
   } = params;
 
   if (type === "instanced") {
-    const configPath = path.join(folder, "config.json");
-    const servicePath = path.join(folder, "namespace.yaml");
-    const deploymentPath = path.join(folder, "ingress.yaml");
-
-    Performer.sendLinkStateEvents(performer, performers, cxt);
-
     const startOp = async (operation, cxt) => {
-      const { operationid } = operation;
 
-      build(operation, params, cxt);
-
-      const watchers = Watcher.multiple(
-        [configPath, servicePath, deploymentPath],
+      await build(params, cxt);
+      /*const watchers = Watcher.multiple(
+        ["config.json", "entities/*"],
         changedPath => {
-          IO.sendEvent(
-            "warning",
-            {
-              data: changedPath + " changed..."
-            },
-            cxt
-          );
-          build(operation, params, cxt);
+          IO.print("warning", changedPath + " changed...", cxt);
+          build(params, cxt);
         }
-      );
+      );*/
 
       while (operation.status !== "stopping") {
-        await wait(100); //wait(2500);
+        await wait(100);
       }
 
-      Watcher.stop(watchers);
-
-      /*IO.sendEvent(
-        "stopped",
-        {
-          operationid,
-          data: ""
-        },
-        cxt
-      );*/
+      //Watcher.stop(watchers);
     };
 
     return {
@@ -149,7 +94,7 @@ export const start = (params, cxt) => {
   }
 };
 
-const build = (operation, params, cxt) => {
+const build = async (params, cxt) => {
   const {
     performer: {
       type,
@@ -161,54 +106,15 @@ const build = (operation, params, cxt) => {
     }
   } = params;
 
-  const { operationid } = operation;
-
   try {
-    IO.sendEvent(
-      "out",
-      {
-        operationid,
-        data: "Start building config..."
-      },
-      cxt
-    );
-
+    IO.print("out", "Start building config...", cxt);
     Config.build(folder);
     const values = Config.load(folder);
 
-    const src = path.join(folder, "");
-    const dest = path.join(folder, "dist");
+    await Cluster.Tasks.Build.exec(folder, values, {}, cxt);
 
-    Cluster.Config.configure("namespace.yaml", src, dest, values);
-    Cluster.Config.configure("ingress.yaml", src, dest, values);
-
-    IO.sendEvent(
-      "out",
-      {
-        data: "Namespace & ingress generated!"
-      },
-      cxt
-    );
-
-    configFolder("secrets", src, dest, values);
-    configFolder("volumes", src, dest, values);
-    configFolder("storages", src, dest, values);
-
-    IO.sendEvent(
-      "done",
-      {
-        data: "Namespace build!"
-      },
-      cxt
-    );
+    IO.print("done", "Namespace build!", cxt);
   } catch (e) {
-    IO.sendEvent(
-      "error",
-      {
-        operationid,
-        data: e.toString()
-      },
-      cxt
-    );
+    IO.print("error", e.toString(), cxt);
   }
 };
--- src/run.jsx
+++ src/run.jsx
@@ -8,8 +8,6 @@ import { IO } from "@nebulario/core-plugin-request";
 import * as Cluster from "@nebulario/core-cluster";
 import * as JsonUtils from "@nebulario/core-json";
 
-import { isFile, getFiles } from "./utils";
-
 export const init = async (params, cxt) => {
   const {
     performers,
@@ -37,243 +35,16 @@ export const init = async (params, cxt) => {
     if (pluginid.startsWith("agent:")) {
       const [agent, type] = pluginid.split(":");
 
-      IO.sendEvent(
-        "info",
-        {
-          data: "Initialize container agent... " + type
-        },
-        cxt
-      );
+      IO.print("info", "Initialize container agent... " + type, cxt);
 
       const targetFolder = "/home/docker/agent/" + instanceid + "/" + type;
       await Cluster.Minikube.copyToHost(
         { path: home, type: "folder" },
         targetFolder,
+        {},
         cxt
       );
     }
-  }
-
-
-};
-
-const runSecrets = async (params, cxt) => {
-  const {
-    command,
-    performers,
-    performer,
-    performer: {
-      type,
-      code: {
-        paths: {
-          absolute: { folder }
-        }
-      }
-    },
-    instance: {
-      instanceid,
-      paths: {
-        absolute: { folder: instanceFolder }
-      }
-    },
-    plugins
-  } = params;
-
-  IO.sendEvent(
-    "info",
-    {
-      data: "Create secrets... " + type
-    },
-    cxt
-  );
-
-  const tmpPath = path.join(folder, "tmp", "secrets");
-  const distPath = path.join(folder, "dist", "secrets");
-  const { cluster } = params.config;
-  const list = getFiles(distPath, "yaml");
-
-  for (const secretPath of list) {
-    const file = path.basename(secretPath);
-    IO.sendEvent(
-      "info",
-      {
-        data: "Secrets... " + file
-      },
-      cxt
-    );
-
-    const rootPath = path.join(
-      cluster.root,
-      "secrets",
-      file.replace(".yaml", "") + ".json"
-    );
-
-    if (fs.existsSync(rootPath)) {
-      const secretDevFile = await Cluster.Dev.transform(
-        file,
-        distPath,
-        tmpPath,
-        async content => {
-          if (cluster) {
-            const actualSecret = JsonUtils.load(rootPath);
-
-            for (const dv in content.data) {
-              if (actualSecret[dv]) {
-                content.data[dv] = Buffer.from(actualSecret[dv]).toString(
-                  "base64"
-                );
-              }
-            }
-          }
-
-          content.metadata.namespace =
-            instanceid + "-" + content.metadata.namespace;
-
-          return content;
-        }
-      );
-
-      const nsout = await Cluster.Control.apply(secretDevFile, cxt);
-      IO.sendOutput(nsout, cxt);
-    }
-  }
-};
-
-const runVolumes = async (params, cxt) => {
-  const {
-    command,
-    performers,
-    performer,
-    performer: {
-      type,
-      code: {
-        paths: {
-          absolute: { folder }
-        }
-      }
-    },
-    instance: {
-      instanceid,
-      paths: {
-        absolute: { folder: instanceFolder }
-      }
-    },
-    plugins
-  } = params;
-
-  IO.sendEvent(
-    "info",
-    {
-      data: "Create volumes... " + type
-    },
-    cxt
-  );
-
-  const tmpPath = path.join(folder, "tmp", "volumes");
-  const distPath = path.join(folder, "dist", "volumes");
-  const { cluster } = params.config;
-  const list = getFiles(distPath, "yaml");
-
-  for (const volPath of list) {
-    const file = path.basename(volPath);
-    IO.sendEvent(
-      "info",
-      {
-        data: "Volumes... " + file
-      },
-      cxt
-    );
-
-    const currVol = JsonUtils.load(volPath, true);
-
-    const rootPath = path.join(
-      cluster.root,
-      "volumes",
-      currVol.spec.awsElasticBlockStore.volumeID
-    );
-
-    if (fs.existsSync(rootPath)) {
-      // COPY TO MINIKUBE HOST
-    }
-    const localDevFile = await Cluster.Dev.transform(
-      file,
-      distPath,
-      tmpPath,
-      async content => {
-        content.metadata.namespace =
-          instanceid + "-" + content.metadata.namespace;
-
-        content.spec.storageClassName = "local-storage";
-        content.spec.capacity.storage = "50Mi";
-        content.spec.hostPath = {
-          path: path.join(
-            "/volumes",
-            instanceid,
-            currVol.spec.awsElasticBlockStore.volumeID
-          )
-        };
-        if (content.spec.awsElasticBlockStore) {
-          delete content.spec.awsElasticBlockStore;
-        }
-        /*
-nodeAffinity:
-  required:
-    nodeSelectorTerms:
-    - matchExpressions:
-      - key: kubernetes.io/hostname
-        operator: In
-        values:
-        - minikube
-*/
-        /*
-
-
-          spec:
-                storageClassName: local-storage
-                accessModes:
-                  - ReadWriteOnce
-                capacity:
-                  storage: 50Mi
-                hostPath:
-                  path: /data/` +
-                  instanceid +
-                  `/` +
-                  performerid +
-                  `/
-                nodeAffinity:
-                  required:
-                    nodeSelectorTerms:
-                    - matchExpressions:
-                      - key: kubernetes.io/hostname
-                        operator: In
-                        values:
-                        - minikube
-
-
-          apiVersion: "v1"
-          kind: "PersistentVolume"
-          metadata:
-            name: certificates
-            namespace: ${NAMESPACE@microservice-config}
-            labels:
-              service: certificate
-          spec:
-            storageClassName: standard
-            capacity:
-              storage: "1Gi"
-            accessModes:
-              - "ReadWriteOnce"
-            awsElasticBlockStore:
-              fsType: "ext4"
-              volumeID: "vol-0572afea6012336e3"
-          */
-
-        return content;
-      }
-    );
-
-    const nsout = await Cluster.Control.apply(localDevFile, cxt);
-    IO.sendOutput(nsout, cxt);
   }
 };
 
@@ -300,92 +71,71 @@ export const start = (params, cxt) => {
   } = params;
 
   const startOp = async (operation, cxt) => {
-    IO.sendEvent(
-      "out",
+    IO.print("out", "Setting namespace config...", cxt);
+
+    const res = await Cluster.Tasks.Run.exec(
+      folder,
       {
-        data: "Setting namespace config..."
-      },
-      cxt
-    );
-
-    const distPath = path.join(folder, "dist");
-    const tmpPath = path.join(folder, "tmp");
-
-    const namespaceDevFile = await Cluster.Dev.transform(
-      "namespace.yaml",
-      distPath,
-      tmpPath,
-      async content => {
-        content.metadata.name = instanceid + "-" + content.metadata.name;
-        return content;
-      }
-    );
-
-    const nsout = await Cluster.Control.apply(namespaceDevFile, cxt);
-    IO.sendOutput(nsout, cxt);
-
-    const ingressDevFile = await Cluster.Dev.transform(
-      "ingress.yaml",
-      distPath,
-      tmpPath,
-      async content => {
-        content.metadata.namespace =
-          instanceid + "-" + content.metadata.namespace;
-
-        content.spec.rules = content.spec.rules.map(rule => {
-          rule.host = instanceid + "-" + rule.host;
-          return rule;
-        });
-
-        for (const rule of content.spec.rules) {
-          const { host } = rule;
-
-          IO.sendEvent(
-            "out",
-            {
-              data: "Add " + host + " namespace to local /etc/hosts..."
-            },
-            cxt
-          );
-          await Cluster.Control.exec(
-            [],
-            async ([], innerClusterContext, cxt) => {
-              const line = "127.0.0.1 " + host;
-              const file = "/etc/hosts";
-
-              return await innerClusterContext(
-                `grep -qF -- "${line}" "${file}" || echo "${line}" >> "${file}"`,
-                {},
-                cxt
-              );
-            },
-            {},
-            cxt
-          );
+        handlers: {
+          onError: async ({ type, file }, e, cxt) => {
+            IO.print("warning", type + " " + file + "  " + e.toString(), cxt);
+          },
+          onCompleted: async ({ type, file }, res, cxt) => {
+            IO.print("info", type + " " + file + " completed", cxt);
+            IO.print("out", res.stdout, cxt);
+            IO.print("warning", res.strerr, cxt);
+          },
+          entities: {}
         }
-
-        return content;
-      }
-    );
-
-    const igsout = await Cluster.Control.apply(ingressDevFile, cxt);
-    IO.sendOutput(igsout, cxt);
-
-    await runSecrets(params, cxt);
-    await runVolumes(params, cxt);
-
-    IO.sendEvent(
-      "info",
-      {
-        data: "Namespace & Ingress up to date..."
       },
+      {},
       cxt
     );
 
-    IO.sendEvent("done", {}, cxt);
+    for (const ing of res.entities["ingress"]) {
+      const {
+        file,
+        paths: { tmp },
+        error
+      } = ing;
+
+      if (error) {
+        continue;
+      }
+
+      const content = JsonUtils.load(path.join(tmp, file), true);
+
+      for (const rule of content.spec.rules) {
+        const { host } = rule;
+
+        IO.print(
+          "out",
+          "Add " + host + " namespace to local /etc/hosts...",
+          cxt
+        );
+
+        await Cluster.Control.exec(
+          [],
+          async ([], innerClusterContext, cxt) => {
+            const line = "127.0.0.1 " + host;
+            const file = "/etc/hosts";
+
+            return await innerClusterContext(
+              `grep -qF -- "${line}" "${file}" || echo "${line}" >> "${file}"`,
+              {},
+              cxt
+            );
+          },
+          {},
+          cxt
+        );
+      }
+    }
+
+    IO.print("done", "Namespace & Ingress up to date...", cxt);
 
     while (operation.status !== "stopping") {
-      await wait(100); //wait(2500);
+      await wait(100);
     }
   };
 
